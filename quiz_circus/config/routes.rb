1475185c0b5f4d049f110e80ec4983c2cf83255f
Sarah Greene <sarahmaudegreene@gmail.com>
2015-04-17 17:24:52 -0400

    w05quiz

--- quiz_circus/config/routes.rb
+++ quiz_circus/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'circuses#index'
   resources :circuses
   resources :entertainers
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
